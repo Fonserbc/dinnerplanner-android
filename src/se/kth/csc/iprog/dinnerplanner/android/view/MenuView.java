b6413f2878acd4935e6a965e2e47dca86bd1c913
Fonserbc <fonserbc@gmail.com>
2014-02-07 17:08:26 +0100

    Working on ingredients list

--- src/se/kth/csc/iprog/dinnerplanner/android/view/MenuView.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/view/MenuView.java
@@ -1,14 +1,24 @@
 package se.kth.csc.iprog.dinnerplanner.android.view;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import se.kth.csc.iprog.dinnerplanner.android.R;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import se.kth.csc.iprog.dinnerplanner.model.Dish;
+import se.kth.csc.iprog.dinnerplanner.model.Ingredient;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MenuView {
 
 	public View view;
-	private DinnerModel model;
+	protected DinnerModel model;
 
 	public MenuView(View view, DinnerModel model) {
 
@@ -18,9 +28,43 @@ public class MenuView {
 
 		//TextView start = (TextView) view.findViewById(R.id.start_page);
 		//example.setText("Hello world");
+		
+		fillInstructionsList();
 
 		// Setup the rest of the view layout
 		
 	}
 
+	private void fillInstructionsList() {
+		ListView list = (ListView) view.findViewById(R.layout.ingredients_list);
+		
+		List<Ingredient> ingredients = retrieveIngredients(model.getFullMenu());
+		
+		for (Ingredient i : ingredients) {
+			
+		}
+	}
+
+	/**
+	 * Returns all ingredients of the current menu, one of each type
+	 */
+	private List<Ingredient> retrieveIngredients(Set<Dish> dishes) {
+		ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+		Map<String,Integer> ingNames = new HashMap<String,Integer>();
+		
+		Set<Ingredient> ing = model.getAllIngredients();
+		
+		for (Ingredient i : ing) {
+			if (ingNames.containsKey(i.getName())) { // If ingredient is in set
+				Ingredient current = ingredients.get(ingNames.get(i.getName()));
+				
+				current.setQuantity(current.getQuantity() + i.getQuantity());
+			}
+			else {
+				
+			}
+		}
+		
+		return ingredients;
+	}
 }
